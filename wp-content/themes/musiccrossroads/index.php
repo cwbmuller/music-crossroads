13aa637eb996e4524d1b5ab6c21154a050957869
Chris Muller <chris@pango.studio>
2019-03-15 10:19:34 +0200

    Add pagination to the blog archive limiting to 6 per page

--- wp-content/themes/musiccrossroads/index.php
+++ wp-content/themes/musiccrossroads/index.php
@@ -10,9 +10,10 @@ if ( class_exists( 'Timber' ) ) {
     $context = Timber::get_context();
     $post = new TimberPost();
     $context['post'] = $post;
+    $page = $_GET['page'];
     $postargs = array(
         'post_type' => 'post',
-        'numberposts' => 99,
+        'numberposts' => -1,
 //        'tax_query' => array(
 //            'relation' => 'AND',
 //            array(
@@ -24,7 +25,7 @@ if ( class_exists( 'Timber' ) ) {
     );
     $context['categories'] = Timber::get_terms('category');
     $context['countries'] = get_terms( 'team_country');
-    $context['news'] = Timber::get_posts($postargs);
+    $context['posts'] = Timber::get_posts($postargs);
     Timber::render(array('template-blog.twig', 'page.twig'), $context);
 
 }
